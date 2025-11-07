05a6b93333ae7d17cc20095775f6bf45c386e813
DanielL689 <DanielL689@nycstudents.net>
2025-11-07 13:04:05 -0500

    lesson 18 demo.py

--- lesson 18 demo.py
+++ lesson 18 demo.py
@@ -9,13 +9,7 @@ turtle.bgcolor("black")
 
 turtle.tracer(False)
 
-def drawSquare(size,size2):
-     for times in range(4):
-         bob.forward(size)
-         bob.right(90)
-         bob.circle(size2)
-         bob.left(180)
-        
+
 def polygon(side,size,c):
      bob.color(c)
      bob.begin_fill()
@@ -25,9 +19,7 @@ def polygon(side,size,c):
           bob.left(angle)
      bob.end_fill()
 
-def circle(size,c):
-     bob.color(c)
-     bob.circle(size)
+
      
 def jump(x,y):
     bob.penup()
@@ -40,30 +32,12 @@ def comet(size,angle,length):
           bob.left(angle)
           bob.width(times)
 
-def flower(c):
-     bob.color(c)
-     bob.begin_fill()
-     for times in range(5):
-         bob.circle(30)
-         bob.left(50)
-         bob.forward(1)
-         
-
-def flower(x,y,c):
-     bob.color(c)
-     jump(x,y)
-     bob.begin_fill()
-     for times in range(5):
-         bob.circle(30)
-         bob.left(1)
-         bob.forward(10)
-
-
 for times in range(254):
      n=randint(0,255)
      c=(n,times,n)
      polygon(9,255-times,c)
      comet(times,1,1)
      bob.left(90)
+
 
 
